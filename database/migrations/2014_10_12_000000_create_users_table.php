d85b4d58f03a6a8f23fc5255e0fc1208f159c387
Masumrahmanhasan <55694939+Masumrahmanhasan@users.noreply.github.com>
2019-10-09 02:00:12 +0600

    Users table seeder added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('phone');
             $table->string('password');
-            $table->bigInteger('reward_points')->default();
+            $table->bigInteger('reward_points')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('email_verification_token', 64)->nullable();
             $table->string('facebook_id', 32)->nullable();
